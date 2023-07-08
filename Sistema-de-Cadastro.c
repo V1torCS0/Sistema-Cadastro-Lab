ca6ecf1db28b01462f7047b52bd53f2e323e8158
Vitor Costa <vitorcostaworks2022@gmail.com>
2023-07-08 15:53:02 -0300

    funções-insertData-e-deleteData-implementadas

--- Sistema-de-Cadastro.c
+++ Sistema-de-Cadastro.c
@@ -6,17 +6,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
 //arquivo padrão para execução do sistema
-#define FILENAME "queriesDB.txt"
+#define FILE_NAME "queriesDB.txt"
+#define MAX_LEN 100
+#define INS 1
+#define DEL 2
+#define UPD 3
+#define CLS 4
 
 typedef struct {
     
-    char * studentName;
+    char studentName[MAX_LEN];
     int studentAge;
     int registration;
-    int schoolGrade;
-    int evaluationScore;
+    int schoolSemester;
+    float evaluationScore;
 } Data;
 
 
@@ -29,28 +34,34 @@ void closeSystem (FILE *);
 
 int main (void) {
 
-    FILE * cursor = fopen (FILENAME, "r");
-    int choice = showMenu ();
+    FILE * cursor;
+    int choice;
 
-    switch (choice) {
+    do {
 
-        case 1:
-            //insertData (cursor);
-            break;
-        case 2:
-            //deleteData (cursor);
-            break;
-        case 3:
-            //updateData (cursor);
-            break;
-        case 4:
-            closeSystem (cursor);
-            break;
+        cursor = fopen (FILE_NAME, "r");
+        choice = showMenu ();
+
+        switch (choice) {
+
+            case INS:
+                insertData (cursor);
+                break;
+            case DEL:
+                deleteData (cursor);
+                break;
+            case UPD:
+                //updateData (cursor);
+                break;
+            case CLS:
+                closeSystem (cursor);
+                break;
     
-        default:
-            printf ("Falha ao executar.\n");
-            break;
-    }
+            default:
+                printf ("Falha ao executar.\n");
+                break;
+        }
+    } while (choice != CLS);
     
     return 0;
 }
@@ -58,13 +69,13 @@ int main (void) {
 int showMenu (void) {
 
     printf ("===Menu do Sistema===\n");
-    printf ("1. Adicionar Elemento;\n"); printf ("2. Deletar Elemento;\n");
-    printf ("3. Editar Elemento;\n"); printf ("4. Fechar Arquivo.\n");
+    printf ("1. Adicionar Cadastro;\n"); printf ("2. Deletar Cadastro;\n");
+    printf ("3. Editar Cadastro;\n"); printf ("4. Fechar Arquivo.\n");
     
     printf ("Input: ");
     int inputValue = pickUp ();
     
-    if (inputValue <= 4 && inputValue >= 1)
+    if (inputValue <= CLS && inputValue >= INS)
         return inputValue;
     else {
         printf ("Tente novamente...\n");
@@ -81,19 +92,76 @@ int pickUp (void) {
 void insertData (FILE * masterFile) {
 
     if (masterFile == NULL)
-        masterFile = fopen (FILENAME, "w");
+        masterFile = fopen (FILE_NAME, "w");
     else
-        masterFile = fopen (FILENAME, "a");
+        masterFile = fopen (FILE_NAME, "a");
 
+    Data student;
+
+    printf ("Insira os dados do aluno (um aluno por vez):");
+
+    printf ("\nNome: "); scanf ("%s", student.studentName);
+    printf ("\nIdade: "); scanf ("%d", &student.studentAge);
+    printf ("\nMatricula: "); scanf ("%d", &student.registration);
+    printf ("\nSemestre: "); scanf ("%d", &student.schoolSemester);
+    printf ("\nNota do Exame: "); scanf ("%f", &student.evaluationScore);
+
+    fprintf (masterFile, "Nome: %s | Idade: %d | Matricula: %d | Semestre: %d | Nota do Exame: %.2f\n",
+    student.studentName, student.studentAge, student.registration, student.schoolSemester, student.evaluationScore);
     
+    rewind (masterFile);
 }
 
 void deleteData (FILE * masterFile) {
 
+    if (masterFile == NULL) {
+
+        printf ("Falha ao abrir arquivo.\n");
+        return;
+    }
+
+    char line[MAX_LEN];
+    char registrationRemoved[7]; int registrationScanned; //padrão de matrícula com 6 dígitos
+    int registrationFound = 0;
+
+    printf ("Digite a matricula do aluno a ser removido: ");
+    scanf ("%d", &registrationScanned); sprintf (registrationRemoved, "%d", registrationScanned);
+
+    FILE * copyAndPaste = fopen ("temp.txt", "w");
+
+    if (copyAndPaste == NULL) {
+
+        printf ("Falha ao criar arquivo auxiliar.\n");
+        return;
+    }
+
+    while (fgets (line, sizeof (line), masterFile) != NULL) {
+        
+        if (strstr(line, registrationRemoved) == NULL)
+            fputs (line, copyAndPaste);
+        else
+            registrationFound++;
+    }
+
+    if (registrationFound)
+        printf ("Removido com sucesso.\n");
+    else
+        printf ("Matricula nao foi cadastrada.\n");
+
+    fclose (masterFile);
+    fclose (copyAndPaste);
+
+    remove (FILE_NAME);
+    rename ("temp.txt", FILE_NAME);
 }
 
 void updateData (FILE * masterFile) {
 
+    if (masterFile == NULL) {
+
+        printf ("Falha ao abrir arquivo.\n");
+        return;
+    }
 }
 
 void closeSystem (FILE * masterFile) {
